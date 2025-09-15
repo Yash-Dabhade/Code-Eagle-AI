d0b9b066e6d68c2a40ba75dd0ad9f0765f382917
Yash-Dabhade <yashdabhade215@gmail.com>
2025-09-15 15:02:11 +0530

    Completed producer end to listen to PR raise webhook event and publish to rabbitMQ with basic info of PR

--- producer/config/rabbitmq.py
+++ producer/config/rabbitmq.py
@@ -1,1 +1,28 @@
 # Singleton rabbitMQ instance
+import pika
+from config.env import env
+
+class RabbitMQ_Singleton:
+    def __init__(self):
+        self.instance = None
+        if not self.instance:
+            self._create_connection()
+
+    def _create_connection(self):
+        credentials = pika.PlainCredentials(env.RABBITMQ_USER, env.RABBITMQ_PASS)
+        self.instance = pika.BlockingConnection(pika.ConnectionParameters(host=env.RABBITMQ_HOST, credentials=credentials))
+
+    def get_connection(self):
+        if not self.instance or self.instance.is_closed:
+            self._create_connection()
+        return self.instance
+
+    def close_connection(self):
+        if self.instance and not self.instance.is_closed:
+            self.instance.close()
+
+rabbitmq_instance = RabbitMQ_Singleton()
+
+
+
+
